2a70ed3df946bb9d7c7cf6b72f4096f8d0d6ab6b
CarliCG <carlicam22@gmail.com>
2023-12-18 20:10:36 -0500

    Interaccion de input y text area en view chat

--- src/views/Chat.js
+++ src/views/Chat.js
@@ -16,7 +16,7 @@ export const ChatView = (name, imageUrl) => {
         </form>
         <div id="pregunta-div">
           <input type="text" id="pregunta-input" placeholder="Pregunta aquí" />
-          <button type="submit" class="button-send">
+          <button type="submit" class="button-send" id="send-button">
             <img src="../images/flecha.png" alt="enviar" />
           </button>
         </div>
@@ -25,6 +25,34 @@ export const ChatView = (name, imageUrl) => {
   `;
 
   itemChat.setAttribute("id", "chat");
+
+  const sendButton = itemChat.querySelector("#send-button");
+  const preguntaInput = itemChat.querySelector("#pregunta-input");
+  const messageText = itemChat.querySelector("#message-text");
+
+  // Agregar evento de submit al formulario
+  const messageForm = itemChat.querySelector("#message-form");
+  messageForm.addEventListener("submit", (event) => {
+    event.preventDefault(); // Evitar recarga de página
+
+    const inputValue = preguntaInput.value;
+    if (inputValue.trim() !== "") { // Verificar que el contenido no sea vacío
+      messageText.value += inputValue + '\n'; // Agregar el nuevo mensaje al text area
+      preguntaInput.value = ""; // Limpiar input
+    }
+  });
+
+  // Enviar la pregunta de input al text area al presionar la tecla enter
+  preguntaInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const inputValue = preguntaInput.value;
+      if (inputValue.trim() !== "") { // Verificar que el contenido no sea vacío
+        messageText.value += inputValue + '\n'; // Agregar el nuevo mensaje al text area
+        preguntaInput.value = ""; // Limpiar input
+      }
+    }
+  });
 
   return itemChat;
 };
