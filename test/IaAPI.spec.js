b6cc007d84ed441a2b63de50775eac2649e36219
CarliCG <carlicam22@gmail.com>
2024-01-04 14:45:41 -0500

    Todos los test positivos

--- test/IaAPI.spec.js
+++ test/IaAPI.spec.js
@@ -9,13 +9,25 @@ global.fetch = jest.fn(() => Promise.resolve({
 
 // Verificar si estamos en un entorno de prueba Jest antes de asignar a globalThis
 describe('Endpoint de openIA', () => {
+  const userMessage = 'hello';
+  const movie = 'YourMovie';
+  const systemMessage = `You are ${movie} therefore respond collectively to all questions you can about the movie`;
+  const messages = [
+    { role: 'system', content: systemMessage },
+    { role: 'user', content: userMessage }
+  ];
+
   it('Integración con la API externa se está realizando correctamente', () => {
     OpenIAResponse.mockResolvedValueOnce({ choices: [{ message: 'hello' }] });
-
-    const messages = [{ role: 'system', content: 'hello' }];
-
-    chatCompletions('12456', messages);
-
+  
+    chatCompletions('12456', userMessage, movie);
+  
+    // Asegurarse de que cada propiedad 'content' esté correctamente serializada
+    const serializedMessages = messages.map(({ role, content }) => ({
+      role,
+      content: content // Usa el contenido directamente sin serializar nuevamente
+    }));
+  
     expect(global.fetch).toBeCalledWith('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       headers: {
@@ -24,11 +36,10 @@ describe('Endpoint de openIA', () => {
       },
       body: JSON.stringify({
         'model': 'gpt-3.5-turbo',
-        messages,
+        messages: serializedMessages,
       })
     });
   });
-
   it('El endpoint responde de manera correcta', () => {
     const response = {
       "choices": [
@@ -48,4 +59,3 @@ describe('Endpoint de openIA', () => {
       });
   });
 });
-
